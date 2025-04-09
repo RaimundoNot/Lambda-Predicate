670a0798fc4bf07004a965e1f1a0028a1132b3be
RaimundoNot <raimundomotadeol@gmail.com>
2025-04-09 07:33:10 -0400

    Criando funções que recebem funções como parâmetro

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -3,9 +3,9 @@ package Program;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
 
 import Entities.Product;
+import model.services.ProductService;
 
 public class Main {
 
@@ -18,11 +18,12 @@ public class Main {
 		 list.add(new Product("Mouse", 50.00));
 		 list.add(new Product("Tablet", 350.50));
 		 list.add(new Product("HD Case", 80.90));
+		  
+		 ProductService ps = new ProductService();
 		 
+		 double sum = ps.filteredSum(list, p -> p.getPrice() < 100.0);
 		 
-		List<String> name = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
-		 
-		name.forEach(System.out::println);
+		 System.out.println("Sum= " + String.format("%.2f", sum));
 	}
 
 }
