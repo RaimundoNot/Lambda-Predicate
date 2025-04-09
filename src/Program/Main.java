68dfe2628025632c92c00866fe9efe77d51512ca
RaimundoNot <raimundomotadeol@gmail.com>
2025-04-09 07:13:38 -0400

    Quarta versão da interface Function, expressão lambda declarada

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -3,6 +3,7 @@ package Program;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import Entities.Product;
@@ -19,7 +20,9 @@ public class Main {
 		 list.add(new Product("Tablet", 350.50));
 		 list.add(new Product("HD Case", 80.90));
 		 
-		List<String> name = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		 Function<Product, String> func = p -> p.getName().toUpperCase();
+		 
+		List<String> name = list.stream().map(func).collect(Collectors.toList());
 		 
 		name.forEach(System.out::println);
 	}
