8f6216902a45278b9124bf161ed6015564be665e
RaimundoNot <raimundomotadeol@gmail.com>
2025-04-09 06:51:45 -0400

    Quinta versão da Interface Consumer, expressão lambda inline

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -3,7 +3,6 @@ package Program;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
 
 import Entities.Product;
 
@@ -19,11 +18,7 @@ public class Main {
 		 list.add(new Product("Tablet", 350.50));
 		 list.add(new Product("HD Case", 80.90));
 		 
-		 Consumer<Product> cons = P -> {
-			 P.setPrice(P.getPrice()*1.1);
-		 };
-		 
-		 list.forEach(cons);
+		 list.forEach(P -> P.setPrice(P.getPrice()*1.1));
 		 
 		 list.forEach(System.out::println);
 	}
