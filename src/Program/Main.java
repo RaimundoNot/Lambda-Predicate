54b1121700a39a2736bcb9c3b255d32d4fef0b07
RaimundoNot <raimundomotadeol@gmail.com>
2025-04-09 07:16:17 -0400

    Quinta versão da interface Funtion, expressão Lambda inline

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -3,7 +3,6 @@ package Program;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import Entities.Product;
@@ -20,9 +19,8 @@ public class Main {
 		 list.add(new Product("Tablet", 350.50));
 		 list.add(new Product("HD Case", 80.90));
 		 
-		 Function<Product, String> func = p -> p.getName().toUpperCase();
 		 
-		List<String> name = list.stream().map(func).collect(Collectors.toList());
+		List<String> name = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 		 
 		name.forEach(System.out::println);
 	}
