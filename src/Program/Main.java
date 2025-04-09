35a2d901101c23fc57caeb17b327dda6c90deab2
RaimundoNot <raimundomotadeol@gmail.com>
2025-04-09 06:49:06 -0400

    Quarta versão utilizando a interface Consumer, com expressão lambda declarada

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -3,6 +3,7 @@ package Program;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import Entities.Product;
 
@@ -18,7 +19,11 @@ public class Main {
 		 list.add(new Product("Tablet", 350.50));
 		 list.add(new Product("HD Case", 80.90));
 		 
-		 list.forEach(Product::nonStaticPriceUpdte);
+		 Consumer<Product> cons = P -> {
+			 P.setPrice(P.getPrice()*1.1);
+		 };
+		 
+		 list.forEach(cons);
 		 
 		 list.forEach(System.out::println);
 	}
